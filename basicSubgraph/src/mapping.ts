a5406e81c9d50c7b42d1d6f9843756bf2927637d
FHieser <felix_hieser@web.de>
2022-10-17 14:40:59 +0200

    Adapting Subgraph Example to new Contract

--- basicSubgraph/src/mapping.ts
+++ basicSubgraph/src/mapping.ts
@@ -1,22 +1,8 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { newData } from '../generated/DataMapping/DataMapping'
+import { Data } from '../generated/schema'
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
-}
-
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
-  }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleNewData(event: newData): void {
+  let data = new Data(event.params.id.toHex());
+  data.data=event.params.data.toI32();
+  data.save()
 }
